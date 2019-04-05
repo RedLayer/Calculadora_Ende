6a1a74a471ad81342fc8b242dc47ab42a4d05c85
RedLayer <pablof.a3f.4@gmail.com>
2019-04-05 14:01:26 +0200

    JavaDoc de calculadoraCLI
    Creacion del javadoc de la CLI de la calculadora

--- CalculadoraEnde/src/interfazCLI/com/CalculadoraCLI.java
+++ CalculadoraEnde/src/interfazCLI/com/CalculadoraCLI.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 
 /**
  * Es la clase que se encarga de crear la calculadora en modo CLI
- * @author Pablo,Aketza,Aaron
+ * @author Pablo
  *
  */
 public class CalculadoraCLI {
@@ -40,7 +40,7 @@ private static void LanzarMenu() {
 		System.out.println("4- Division		5- Valor absoluto		6- potencia");
 		System.out.println("7- Binario		8- Raiz				9- Logaritmo");
 		System.out.println("\t\t\t0- Salir");
-		System.out.print("Por favor seleccione una operacion insertando uno de los numeros de arriba: ");
+		System.out.print("Por favor seleccione una operacion insertando uno de los números de arriba: ");
 		seleccion=leeInt();
 		switch(seleccion) {
 		  case 1:
@@ -73,7 +73,7 @@ private static void LanzarMenu() {
 		  case 0:
 			break;
 		  default:
-			System.out.println("Numero no valido");
+			System.out.println("Número no valido");
 			break;
 		}
 	}
@@ -82,8 +82,8 @@ private static void LanzarMenu() {
 }
 
 /**
- * Devuelve el numero que introduce el usuario por teclado
- * @return Numero introducido por el usuario en forma de entero
+ * Devuelve el número que introduce el usuario por teclado
+ * @return Número introducido por el usuario en forma de entero
  */
 public static int leeInt() {
 	
@@ -100,84 +100,120 @@ public static int leeInt() {
 
 }
 
+/**
+ * Pide dos números al usuario para realizar una suma entre ellos
+ * @return Devuelve un entero con la suma de los números
+ */
 private static int sumar() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el primer número: ");
 	int num1 = leeInt();
-	System.out.print("Inserta el segundo numero: ");
+	System.out.print("Inserta el segundo número: ");
 	int num2 = leeInt();
 	
 	return calc.suma(num1,num2);
 }
 
+/**
+ * Pide dos números al usuario para realizar una resta entre ellos
+ * @return Devuelve el resultado de la resta de los números en un entero
+ */
 private static int restar() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el primer número: ");
 	int num1 = leeInt();
-	System.out.print("Inserta el segundo numero: ");
+	System.out.print("Inserta el segundo número: ");
 	int num2 = leeInt();
 	
 	return calc.resta(num1,num2);
 }
 
+/**
+ * Pide dos números al usuario para realizar una multiplicación entre ellos
+ * @return Devuelve el resultado de la multiplicación de los números en un entero
+ */
 private static int multiplicar() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el primer número: ");
 	int num1 = leeInt();
-	System.out.print("Inserta el segundo numero: ");
+	System.out.print("Inserta el segundo número: ");
 	int num2 = leeInt();
 	
 	return calc.multi(num1,num2);
 }
 
+/**
+ * Pide dos números al usuario para realizar una división entre ellos
+ * @return Devuelve el resultado de la división de los números en un entero
+ */
 private static double dividir() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el primer número: ");
 	int num1 = leeInt();
-	System.out.print("Inserta el segundo numero: ");
+	System.out.print("Inserta el segundo número: ");
 	int num2 = leeInt();
 	
 	return calc.division(num1,num2);
 }
 
+/**
+ * Pide un número al usuario para sacar el valor absoluto del mismo
+ * @return Devuelve el valor absoluto en un entero
+ */
 private static int abs() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el numero para pasar al valor absoluto: ");
+	System.out.print("Inserta el número para pasar al valor absoluto: ");
 	int num1 = leeInt();
 	return calc.valorAbsoluto(num1);
 }
 
+/**
+ * Pide dos número (número y potencia) y realiza la ecuación
+ * @return Devuelve el número resultante de la potencia en un entero
+ */
 private static int pot() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el número: ");
 	int num1 = leeInt();
-	System.out.print("Inserta el segundo numero: ");
+	System.out.print("Inserta la potencia: ");
 	int num2 = leeInt();
 	
 	return calc.potencia(num1,num2);
 }
 
+/**
+ * Pide un número y lo pasa a binario
+ * @return Devuelve un String con el número en binario
+ */
 private static String bin() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el número que pasar a binario: ");
 	int num1 = leeInt();
 	
 	return calc.pasarABinario(num1);
 }
 
+/**
+ * Pide un número y devuelve la raiz del mismo
+ * @return Devuelve la raiz del número
+ */
 private static float rz() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el número al que hacerle la raiz: ");
 	int num1 = leeInt();
 	
 	return calc.raiz(num1);
 }
 
+/**
+ * Pide un número y una base y saca el logaritmo
+ * @return Devuelve el logaritmo del número en base a la pedida por teclado
+ */
 private static double log() {
 	Calculadora_Potencias calc = new Calculadora_Potencias();
-	System.out.print("Inserta el primer numero: ");
+	System.out.print("Inserta el número: ");
 	int num1 = leeInt();
-	System.out.print("Inserta el segundo numero: ");
+	System.out.print("Inserta la base: ");
 	int num2 = leeInt();
 	
 	return calc.log(num1,num2);
